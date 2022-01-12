86fb6c6ea5399360b01ec038a027c7a4f4920880
Ivan Kuznetsov <ivan.kuznetsov@externals.adidas.com>
2022-01-12 21:04:55 +0200

    Lesson 4 tasks added

---
+++ lessons/src/com/example/java/lesson4/Task1.java
@@ -0,0 +1,24 @@
+package com.example.java.lesson4;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        //TODO print all values divisible by 7 in the range from 1 to 100
+
+
+
+        for (int i = 1; i <= 100; i++   ) {
+            if (i%7==0) {
+                System.out.print(i);
+                System.out.print(", ");
+            }
+        }
+
+
+        //TODO find my favourite number in an Array of ints (for)
+
+
+        //TODO the same with Array and Foreach
+    }
+}
---
+++ lessons/src/com/example/java/lesson4/Task2.java
@@ -0,0 +1,19 @@
+package com.example.java.lesson4;
+
+public class Task2 {
+    public static void main(String[] args) {
+        //TODO while i < 10, print number (while and do while)
+        int i = 0;
+
+//        while ( i <= 10) {
+//            System.out.println(i);
+//            i++;
+//        }
+//
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 10);
+    }
+}
